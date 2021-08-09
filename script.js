0a9e4a3cc37ef1b82c537b90106f61264b563d1b
a.stevens <ata_@hotmail.fr>
2021-08-09 09:20:52 +0200

    affichage du nombre de cellule en vie +le nombre d'iteration qu'il a eux

--- script.js
+++ script.js
@@ -3,35 +3,46 @@ let ctx = canvas.getContext('2d');
 let list = [];
 let listTemp = [];
 let timer
+let year
+let numberCellule
+
 startDraw();
-timer = setInterval(function () { draw() }, 100)
-draw() 
+timer = setInterval(function () {
+    draw();
+    let yearBalise = document.getElementById('year');
+    yearBalise.innerText = year;
+    let number = document.getElementById('numberCellule');
+    number.innerText = numberCellule
+}, 100)
+
 
 function Carre(x, y, life, size) {
     this.x = x;
     this.y = y;
     this.life = life;
     this.color = (life ? "blue" : "white");
-    this.age=0;
+    this.age = 0;
     this.draw = function () {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, size, size);
     }
 }
 
-function startDraw(size=10) {
+function startDraw(size = 10) {
     var width = canvas.width;
     var height = canvas.height;
     let life;
     list = [];
     listTemp = [];
-    
+    year = 0;
+    numberCellule = 0;
     for (let i = 0; i < width; i += size) {
 
         list[i / size] = []
         listTemp[i / size] = []
         for (let j = 0; j < height; j += size) {
             if (Math.floor(Math.random() * 2) == 0) { life = true } else { life = false }
+            if (life) { numberCellule++ }
             list[i / size].push(new Carre(i, j, life, size));
             listTemp[i / size].push(new Carre(i, j, life, size));
             list[i / size][j / size].draw();
@@ -39,40 +50,44 @@ function startDraw(size=10) {
     }
 }
 
-function yearDead(cellule){
-    if(!cellule.life){
+function yearDead(cellule) {
+    if (!cellule.life) {
         cellule.age--
-        switch(cellule.age){
+        switch (cellule.age) {
             case -1:
-                cellule.color="#485EEA"
+                cellule.color = "#485EEA"
                 break;
             case -2:
-                cellule.color="#FFFFFF"
+                cellule.color = "#FFFFFF"
                 break;
         }
     }
- 
+
 }
 
 
 
 function draw() {
-    
+    year++;
+    numberCellule = 0;
     for (let i = 0; i < list.length; i++) {
-        
+
         for (let j = 0; j < list[i].length; j++) {
-            
+
             let nbLife = NbLife(i, j);
 
-            if(nbLife<2 ||nbLife>3){
+            if (nbLife < 2 || nbLife > 3) {
                 listTemp[i][j].life = false;
                 yearDead(listTemp[i][j]);
                 listTemp[i][j].draw()
-            }else if(nbLife === 3){
-                listTemp[i][j].age=0;
+            } else if (nbLife === 3) {
+                listTemp[i][j].age = 0;
                 listTemp[i][j].life = true;
                 listTemp[i][j].color = "blue";
                 listTemp[i][j].draw()
+            }
+            if (listTemp[i][j].life) {
+                numberCellule++;
             }
         }
     }
@@ -88,22 +103,22 @@ function draw() {
 
 function NbLife(i, j) {
     let nbLife = 0;
-    let  tab=[];
+    let tab = [];
 
     tab.push(check(i - 1, list.length, j - 1, list[i].length))
-    tab.push( check(i - 1, list.length, j, list[i].length))
-    tab.push( check(i - 1, list.length, j + 1, list[i].length))
-    tab.push( check(i, list.length, j - 1, list[i].length))
-    tab.push( check(i, list.length, j + 1, list[i].length))
+    tab.push(check(i - 1, list.length, j, list[i].length))
+    tab.push(check(i - 1, list.length, j + 1, list[i].length))
+    tab.push(check(i, list.length, j - 1, list[i].length))
+    tab.push(check(i, list.length, j + 1, list[i].length))
     tab.push(check(i + 1, list.length, j - 1, list[i].length))
-    tab.push( check(i + 1, list.length, j, list[i].length))
-    tab.push( check(i + 1, list.length, j + 1, list[i].length))
+    tab.push(check(i + 1, list.length, j, list[i].length))
+    tab.push(check(i + 1, list.length, j + 1, list[i].length))
 
-   for(let i =0 ;i<tab.length&&nbLife!=4;i++){
+    for (let i = 0; i < tab.length && nbLife != 4; i++) {
         if (list[tab[i][0]][tab[i][1]].life) {
-            
+
             nbLife++;
-        }       
+        }
     }
     return nbLife;
 }
@@ -128,59 +143,92 @@ function check(i, iLenght, j, jLenght) {
 }
 
 
-let btn = document.getElementById("btn");
+let btn = document.getElementById("btnValidation");
 
 btn.addEventListener("click", (e) => {
-    let size =10;
+    let size = 10;
     let sizePixel = document.getElementById("sizePixel")
     if (sizePixel.value >= 1 && sizePixel.value <= 100) {
         size = parseInt(sizePixel.value)
     }
     let sizeHeight = document.getElementById("height")
     let sizeWidth = document.getElementById("width")
-    if(sizeHeight.value>=100 ){
-        canvas.height=sizeHeight.value
+    if (sizeHeight.value >= 100) {
+        canvas.height = sizeHeight.value
     }
-    if(sizeWidth.value>=100 ){
-        canvas.width=sizeWidth.value
+    if (sizeWidth.value >= 100) {
+        canvas.width = sizeWidth.value
     }
     let sizeTimer = document.getElementById("timer")
-    let chrono=100
-    if(sizeTimer.value>=10 ){
-        chrono= sizeTimer.value
+    let chrono = 100
+    if (sizeTimer.value >= 10) {
+        chrono = sizeTimer.value
     }
     clearInterval(timer);
     startDraw(size);
-    timer= setInterval(function () { draw() }, chrono)
+
+    timer = setInterval(function () {
+        draw();
+        let yearBalise = document.getElementById('year');
+        yearBalise.innerText = year;
+        let number = document.getElementById('numberCellule');
+        number.innerText = numberCellule
+    }, chrono)
 })
 
 let sizePixelId = document.getElementById("sizePixel");
-sizePixelId.addEventListener("input",(e)=>{
-    let sizePixelSpan= document.getElementById("sizePixelSpan")
-    sizePixelSpan.innerText=sizePixelId.value
+sizePixelId.addEventListener("input", (e) => {
+    let sizePixelSpan = document.getElementById("sizePixelSpan")
+    sizePixelSpan.innerText = sizePixelId.value
 })
 
 let heightId = document.getElementById("height");
-heightId.addEventListener("input",(e)=>{
-    let heightSpan= document.getElementById("heightSpan")
-    heightSpan.innerText=heightId.value
+heightId.addEventListener("input", (e) => {
+    let heightSpan = document.getElementById("heightSpan")
+    heightSpan.innerText = heightId.value
 })
 
 let WidthId = document.getElementById("width");
-WidthId.addEventListener("input",(e)=>{
-    let WidthSpan= document.getElementById("WidthSpan")
-    WidthSpan.innerText=WidthId.value
+WidthId.addEventListener("input", (e) => {
+    let WidthSpan = document.getElementById("WidthSpan")
+    WidthSpan.innerText = WidthId.value
 })
 
 let timerId = document.getElementById("timer");
-timerId.addEventListener("input",(e)=>{
-    let timerSpan= document.getElementById("timerSpan")
-    timerSpan.innerText=timerId.value
+timerId.addEventListener("input", (e) => {
+    let timerSpan = document.getElementById("timerSpan")
+    timerSpan.innerText = timerId.value
 })
 
-canvas.addEventListener("mousedown",(e)=>{
-    let sizePixel = document.getElementById("sizePixel").value; 
-    list[Math.trunc(e.x/sizePixel)][Math.trunc(e.y/sizePixel)].life=true
-    list[Math.trunc(e.x/sizePixel)][Math.trunc(e.y/sizePixel)].color="blue"
-    list[Math.trunc(e.x/sizePixel)][Math.trunc(e.y/sizePixel)].draw()
+canvas.addEventListener("mousedown", (e) => {
+    const nav = document.querySelector('nav');
+    let sizePixel = document.getElementById("sizePixel").value;
+    list[Math.trunc(e.x / sizePixel)][Math.trunc((e.y - nav.scrollHeight) / sizePixel)].life = true
+    list[Math.trunc(e.x / sizePixel)][Math.trunc((e.y - nav.scrollHeight) / sizePixel)].color = "blue"
+    list[Math.trunc(e.x / sizePixel)][Math.trunc((e.y - nav.scrollHeight) / sizePixel)].draw()
 })
+
+const modal =
+    document.querySelector('.modal');
+const btnModal =
+    document.querySelector('#btn')
+const close =
+    document.querySelector('.modal-close')
+
+btnModal.addEventListener('click',
+    function () {
+        modal.style.display = 'block'
+    })
+
+close.addEventListener('click',
+    function () {
+        modal.style.display = 'none'
+    })
+
+window.addEventListener('click',
+    function (event) {
+        if (event.target.className ===
+            'modal-background') {
+            modal.style.display = 'none'
+        }
+    })
